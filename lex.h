ba45b7e1cb69e694d2fd96ec4dc0098d324a1aa6
eignnx <eignnx@gmail.com>
2021-12-28 22:49:55 -0500

    add parsing for int and char literals

--- lex.h
+++ lex.h
@@ -436,3 +436,26 @@ void lex_all_input(char *input)
     }
     printf("End of input\n");
 }
+
+void test_lexer()
+{
+    char *input =
+        "#include <stdio.h>\n"
+        "#include \"my_file.h\"\n"
+        "\n"
+        "struct my_type {\n"
+        "    int x;\n"
+        "};\n"
+        "\n"
+        "void my_func123()\n"
+        "{ // this is a comment! \n"
+        "    bool my_bool = true;\n"
+        "    int my_int = -1234;\n"
+        "    char my_char1 = '\\n';\n"
+        "    char my_char2 = 'A';\n"
+        "    char my_char3 = '\\'';\n"
+        "    char *my_str = \"qwea sdf sgd \\\" \\n rty\";\n"
+        "}\n";
+
+    lex_all_input(input);
+}
--- paracc.c
+++ paracc.c
@@ -1,26 +1,88 @@
-#include "lex.h" // lex_all_input
+#include <stdio.h>  // fprintf_s, FILE, stdout
+#include <stdlib.h> // atoi, malloc
+#include "lex.h"    // lex, lex_all_input
 
 // Parasitic C
 
+struct AstNode
+{
+    int tok_typ;
+};
+
+int AST_LITERAL_INT = 100;
+struct LiteralIntNode
+{
+    struct AstNode ast;
+    int value;
+};
+
+int dbg_literal_int(FILE *out, struct LiteralIntNode *node)
+{
+    return fprintf_s(out, "%d", node->value);
+}
+
+bool parse_literal_int(char *input, struct LiteralIntNode **node, char **new_input)
+{
+    int tok_typ;
+    char *token;
+    bool result = lex(input, &tok_typ, &token, new_input);
+
+    if (!result || tok_typ != TOK_LITERAL_INT)
+        return false;
+
+    *node = malloc(sizeof(**node));
+    (*node)->value = atoi(token);
+    (*node)->ast.tok_typ = AST_LITERAL_INT;
+    return true;
+}
+
+int AST_LITERAL_CHAR = 101;
+struct LiteralCharNode
+{
+    struct AstNode ast;
+    char *value;
+};
+
+int dbg_literal_char(FILE *out, struct LiteralCharNode *node)
+{
+    return fprintf_s(out, "'%s'", node->value);
+}
+
+bool parse_literal_char(char *input, struct LiteralCharNode **node, char **new_input)
+{
+    int tok_typ;
+    char *token;
+    bool result = lex(input, &tok_typ, &token, new_input);
+
+    if (!result || tok_typ != TOK_LITERAL_CHAR)
+        return false;
+
+    *node = malloc(sizeof(**node));
+    (*node)->value = token;
+    (*node)->ast.tok_typ = AST_LITERAL_CHAR;
+    return true;
+}
+
 int main()
 {
-    char *input =
-        "#include <stdio.h>\n"
-        "#include \"my_file.h\"\n"
-        "\n"
-        "struct my_type {\n"
-        "    int x;\n"
-        "};\n"
-        "\n"
-        "void my_func123()\n"
-        "{ // this is a comment! \n"
-        "    bool my_bool = true;\n"
-        "    int my_int = -1234;\n"
-        "    char my_char1 = '\\n';\n"
-        "    char my_char2 = 'A';\n"
-        "    char my_char3 = '\\'';\n"
-        "    char *my_str = \"qwea sdf sgd \\\" \\n rty\";\n"
-        "}\n";
+    char *new_input;
 
-    lex_all_input(input);
+    {
+        struct LiteralIntNode *node;
+        if (parse_literal_int("-531", &node, &new_input))
+        {
+            bool _ = dbg_literal_int(stdout, node);
+        }
+    }
+
+    printf("\n");
+
+    {
+        struct LiteralCharNode *node;
+        if (parse_literal_char("'\\n'", &node, &new_input))
+        {
+            bool _ = dbg_literal_char(stdout, node);
+        }
+    }
+    printf("\n%s\n", "DONE!");
 }
