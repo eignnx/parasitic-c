feef139b5880378e9a7274e79618b548d3a296ad
eignnx <eignnx@gmail.com>
2021-12-29 19:43:38 -0500

    parse #include

--- paracc.c
+++ paracc.c
@@ -6,10 +6,14 @@
 
 enum AstTag
 {
+    // Literals
     AST_LITERAL_INT,
     AST_LITERAL_CHAR,
     AST_LITERAL_STRING,
     AST_LITERAL_BOOL,
+
+    // Top-level Items
+    AST_POUND_INCLUDE,
 };
 
 char *ast_tag_names[] = {
@@ -17,6 +21,7 @@ char *ast_tag_names[] = {
     "AST_LITERAL_CHAR",
     "AST_LITERAL_STRING",
     "AST_LITERAL_BOOL",
+    "AST_POUND_INCLUDE",
 };
 
 // Example use:
@@ -46,13 +51,21 @@ struct AstNode
             bool value;
         } literal_bool;
 
+        struct
+        {
+            char *filename;
+            enum
+            {
+                ANGLE_BRACKET_POUND_INCLUDE,
+                DOUBLE_QUOTE_POUND_INCLUDE,
+            } kind;
+        } pound_include;
+
     } as;
 };
 
-int dbg_ast_node(FILE *out, struct AstNode *node)
+int display_ast_node(FILE *out, struct AstNode *node)
 {
-    fprintf_s(out, "%s: ", ast_tag_names[node->tag]);
-
     switch (node->tag)
     {
     case AST_LITERAL_INT:
@@ -70,10 +83,27 @@ int dbg_ast_node(FILE *out, struct AstNode *node)
         {
             return fprintf_s(out, "false");
         }
+    case AST_POUND_INCLUDE:
+        fprintf_s(out, "#include ");
+        if (node->as.pound_include.kind == ANGLE_BRACKET_POUND_INCLUDE)
+        {
+            return fprintf_s(out, "<%s>", node->as.pound_include.filename);
+        }
+        else
+        {
+            return fprintf_s(out, "\"%s\"", node->as.pound_include.filename);
+        }
+
     default:
         perror("Invalid AstTag");
         exit(1);
     }
+}
+
+int dbg_ast_node(FILE *out, struct AstNode *node)
+{
+    fprintf_s(out, "%s: ", ast_tag_names[node->tag]);
+    return display_ast_node(out, node);
 }
 
 bool parse_literal_int(char *input, struct AstNode **node, char **new_input)
@@ -151,6 +181,43 @@ bool parse_literal_bool(char *input, struct AstNode **node, char **new_input)
     return false;
 }
 
+bool parse_pound_include(char *input, struct AstNode **node, char **new_input)
+{
+    int tok_tag;
+    char *token;
+    bool result;
+
+    *new_input = input;
+
+    result = lex(*new_input, &tok_tag, &token, new_input);
+
+    if (!result || tok_tag != TOK_POUND)
+        return false;
+
+    result = lex(*new_input, &tok_tag, &token, new_input);
+
+    if (!result || tok_tag != TOK_INCLUDE)
+        return false;
+
+    result = lex(*new_input, &tok_tag, &token, new_input);
+
+    if (result && (tok_tag == TOK_ANGLE_BRACK_FILENAME || tok_tag == TOK_LITERAL_STRING))
+    {
+        *node = malloc(sizeof(**node));
+        (*node)->tag = AST_POUND_INCLUDE;
+        (*node)->as.pound_include.filename = token;
+
+        if (tok_tag == TOK_ANGLE_BRACK_FILENAME)
+            (*node)->as.pound_include.kind = ANGLE_BRACKET_POUND_INCLUDE;
+        else
+            (*node)->as.pound_include.kind = DOUBLE_QUOTE_POUND_INCLUDE;
+
+        return true;
+    }
+
+    return false;
+}
+
 int main()
 {
     char *new_input;
@@ -177,6 +244,16 @@ int main()
     printf("\n");
 
     if (parse_literal_bool("false", &node, &new_input))
+        dbg_ast_node(stdout, node);
+
+    printf("\n");
+
+    if (parse_pound_include("#include <stdio.h>", &node, &new_input))
+        dbg_ast_node(stdout, node);
+
+    printf("\n");
+
+    if (parse_pound_include("#include \"stdio.h\"", &node, &new_input))
         dbg_ast_node(stdout, node);
 
     printf("\n%s\n", "DONE!");
