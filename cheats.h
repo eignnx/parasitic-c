83812c006c2be867d90e9197406f9212c55eb313
eignnx <eignnx@gmail.com>
2022-01-07 00:22:56 -0500

    upgrade unimplemented macro

--- cheats.h
+++ cheats.h
@@ -1,6 +1,8 @@
 #ifndef STD_PARAC_H
 #define STD_PARAC_H
 
+#include <stdio.h> // fprintf, stderr
+
 // A file for putting temporary C code that can't be parsed
 // by the Parasitic C compiler. This file can be included as
 // a C header file and the definitions used in Parasitic C
@@ -8,8 +10,9 @@
 
 typedef char *cstr_arr[];
 
-#define unimplemented                                                          \
-    printf("%s:%d (%s) not yet implemented!\n", __FILE__, __LINE__, __func__); \
+#define unimplemented                                                     \
+    fprintf(stderr, "\n%s:%d: Function `%s` is not yet implemented!\n\n", \
+            __FILE__, __LINE__, __func__);                                \
     exit(1)
 
 #endif // STD_PARAC_H
