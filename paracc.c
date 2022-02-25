b8157e9c1d22a5130859519ce6e6c28a281c7776
eignnx <eignnx@gmail.com>
2022-02-24 22:22:40 -0500

    rename `data` field in List to `value` to avoid kw conflict

---
+++ paracc.c
@@ -0,0 +1,16 @@
+#ifndef PARASITIC_C_FILE__3E8
+#define PARASITIC_C_FILE__3E8
+
+// DIRECTIVES //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "parse.c"
+#include "lex.c"
+#include "test.c"
+#include "cstr_buf.c"
+#include "list.c"
+#include "cheats.h"
+
+// STRUCT DECLS //
+struct Compiler;
